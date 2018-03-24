214cd44d05bef75cc145faf601e5bad17b60d26d
Weijingz <33884435+Weijingz@users.noreply.github.com>
2018-03-23 21:50:47 -0700

    first time commit

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+twitter_consumer_key = "z1wEInAmYKFfNozhwoHfl9mL2"
+twitter_consumer_secret = "ujquYkMOdV8JoG0GNEOiY8aRDTGuuZADNtiR39lHbpZHw2gXDH"
+twitter_access_token = "973684725528256512-5jf1sIL4SDxu7j6CuS6W9u7f9ovgTOv"
+twitter_access_token_secret = "0lD9L0xwNitKJY8xmqeiYQOZPoFSuCyyBVNZDfapSYZQx"
